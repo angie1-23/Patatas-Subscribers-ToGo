512d321a49b849b01c087492230ec86d5701f1bc
Angie Muñoz <aemunozpl@gmail.com>
2023-06-13 14:56:40 -0500

    form login

---
+++ angular-patatas/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public formLogin!:FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formLogin=this.formBuilder.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]]
+    })
+  }
+
+  send(){
+    console.log(this.formLogin.value)
+  }
+
+
+
+}
